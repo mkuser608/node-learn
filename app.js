ba97a3273244d9e7ce26e075cedcd5354d985681
M K Mandal <mkuser608@gmail.com>
2024-05-02 06:51:53 +0530

    Global Objects

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-function sayHello(name) {
-  console.log("Hello ", name);
-}
+//global objects
+console.log();
 
-sayHello("Mukesh");
+setTimeout();
+clearTimeout();
+
+setInterval();
+clearInterval();
+
+window.console.log();
+
+//all the above global objects set to window object or we can call it directly
+
+var message = "";
+window.message; // if we declare a variable that variable can be accessed by window object
+
+// one more object we have global and we can access all above via global
+globalThis.setTimeout(); // but variable we defined like message is not defined at global object
